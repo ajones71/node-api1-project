80274abb59d76826ffe5fec10c65d8bf8eda4ef7
Antoinette Jones <aajones71@gmial.com>
2020-09-02 22:49:08 -0400

    create CRUD endpoints

--- index.js
+++ index.js
@@ -4,8 +4,51 @@ const server = express()
 
 const PORT = 5000
 
+const users =[
+{
+    id: "a_unique_id", // hint: use the shortid npm package to generate it
+    name: "Jane Doe", // String, required
+    bio: "Not Tarzan's Wife, another Jane",  // String, required
+  }
+]
+server.get('/api/users', (req, res) => {
+    res.json(users);
+})
 
+server.get('/api/users/:id', (req, res) => {
+    res.json(users)
+})
+server.post('/api/users', (req, res) =>{
+    const UsersInfo = req.body;
+    UsersInfo.id = shortid.generate()
+    Users.push(UsersInfo);
+    res.status(201).json(UsersInfo)
+})
 
-server.listen(Port, () => {
+server.put('/api/users/:id', (req, res) => {
+ const {id} = req.params;
+ const changes = req.body;
+
+ let index = users.findIndex(user => user.id ===id);
+
+ if (index !==-1){
+     users[index] =changes;
+     res.status(200).json(users[index])
+ }else{
+     res.status(400).json({message: "User Not Found"})
+ }
+})
+
+server.delete('/api/hubs/:id', (req, res) => {
+    const { id } = req.params;
+    const deleted = hubs.find(hub => hub.id === id);
+    if (deleted) {
+        hubs = hubs.filter(hub => hub.id !== id);
+        res.status(200).json(deleted);
+    } else {
+        res.status(404).json({message: "id not found"});
+    }
+});
+server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
